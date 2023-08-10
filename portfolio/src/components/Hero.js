8d980a52d092275c51795c30d475c2c510e76565
Jacob Templeton <jacob.templeton@gmail.com>
2023-08-10 13:18:42 -0700

    Add SVG for Hero component background, SVG styling

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -1,8 +1,32 @@
 import "./Hero.css";
-const Hero = () => {
+const Hero = ({ darkMode }) => {
   return (
     <div className="hero">
       <h1>Hi, I'm Jacob Templeton</h1>
+      <svg
+        className={darkMode ? "background--custom dark" : "background--custom"}
+        viewBox="0 0 100 100"
+        preserveAspectRatio="none"
+      >
+        <path
+          fill="#FFFF00"
+          fillOpacity="0.7"
+          d="M-100 -100L200 -100L200 50L-100 50Z"
+          style={{ animation: "path0 5s linear infinite alternate" }}
+        />
+        <path
+          fill="#00FFFF"
+          fillOpacity="0.7"
+          d="M-100 -100L200 -100L200 50L-100 50Z"
+          style={{ animation: "path1 9.5s linear infinite alternate" }}
+        />
+        <path
+          fill="#FF00FF"
+          fillOpacity="0.2"
+          d="M-100 -100L200 -100L200 20L-100 20Z"
+          style={{ animation: "path2 11s linear infinite alternate" }}
+        />
+      </svg>
     </div>
   );
 };
