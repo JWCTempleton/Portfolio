a4d4b4748884961d9957bd44faf317d1a3901fe3
Jacob Templeton <jacob.templeton@gmail.com>
2023-08-13 15:14:27 -0700

    Add cta component responsive styling

--- portfolio/src/components/Cta.js
+++ portfolio/src/components/Cta.js
@@ -1,4 +1,5 @@
 import { Fade } from "react-awesome-reveal";
+import "./Cta.css";
 
 const Cta = ({ darkMode }) => (
   <div className={darkMode ? "cta dark" : "cta"}>
@@ -7,9 +8,10 @@ const Cta = ({ darkMode }) => (
         <h2 className="call-title">Contact</h2>
 
         <p className="call-content">
-          Please feel free to contact me, I'd love to talk more.
+          Please feel free to contact me,
+          <br /> I'd love to talk more.
         </p>
-        <p className="call-content">jacob.templeton@gmail.com</p>
+        <p className="call-content email">jacob.templeton@gmail.com</p>
       </Fade>
     </div>
   </div>
